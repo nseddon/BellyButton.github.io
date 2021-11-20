9928bfd385dd096feee2e5d7a11c1423bea9d523
Nick Seddon <nickseddon187@gmail.com>
2021-11-19 19:40:21 -0500

    Deliverable 1 complete

--- js/charts.js
+++ js/charts.js
@@ -58,30 +58,54 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    var jsSamples = data.samples;
+    
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var sampleArray = jsSamples.filter(sampleObj => sampleObj.id == sample);
+    
     //  5. Create a variable that holds the first sample in the array.
-
+    var individualResults = sampleArray[0];
+    console.log(individualResults);
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otuIds = individualResults.otu_ids;
+    console.log(otuIds);
+    var otuLabels = individualResults.otu_labels;
+    console.log(otuLabels);
+    var sampleValues = individualResults.sample_values;
+    console.log(sampleValues);
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
-
-    var yticks = 0;
-
-    // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
-    // 9. Create the layout for the bar chart. 
-    var barLayout = {
-     
-    };
-    // 10. Use Plotly to plot the data with the layout. 
+    var yticks = otuIds.map(object => "OTU "+object).slice(0,10).reverse();
+    console.log(yticks);
+    var slicedSampleValues = sampleValues.map(object => object).slice(0,10).reverse();
+    console.log(slicedSampleValues);
     
+    // 8. Create the trace for the bar chart. 
+    var barData = {
+      x: slicedSampleValues,
+      y: yticks,
+      text: otuLabels,
+      type: "bar",
+      orientation: "h"
+    };
+
+    var plotData = [barData];
+    
+    // // 9. Create the layout for the bar chart. 
+    var barLayout = {
+      title: "Greek gods search results",
+      margin: {
+        l: 100,
+        r: 150,
+        t: 100,
+        b: 100
+      }
+    };
+
+    // // 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar", plotData, barLayout);
   });
 }
